bcc26bed8c4c2d1abc31f6f697b710c9e6b41f78
Kengo Nakajima <kengo.nakajima@gmail.com>
2021-07-27 08:27:56 +0900

    worked

--- signaling-server.js
+++ signaling-server.js
@@ -15,7 +15,7 @@ var server = http.createServer(main)
 var io  = require('socket.io').listen(server);
 //io.set('log level', 2);
 
-server.listen(process.env.PORT || 5000, null, function() {
+server.listen(PORT, null, function() {
     console.log("Listening on port " + PORT);
 });
 //main.use(express.bodyParser());
